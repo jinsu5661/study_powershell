96c3f727824012252778bac27e5f266879b6b673
jinsu5661 <45279355+jinsu5661@users.noreply.github.com>
2022-01-28 09:26:10 +0900

    Update get-NetTcpConnection.ps1

--- get-NetTcpConnection.ps1
+++ get-NetTcpConnection.ps1
@@ -20,6 +20,5 @@ Foreach($i in $connections){
         $i
     }
 }
-![image](https://user-images.githubusercontent.com/45279355/151465305-925b5a7e-85ad-4621-87e2-c805cfddd4d0.png)
-
+![1](https://user-images.githubusercontent.com/45279355/151465598-82f82e85-9653-449e-9d21-e96cb04839f8.png)
 
